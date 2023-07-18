faa0eec0d3f2a867d08434d4881eb423f9282980
Scott549 <g3107857740@gmail.com>
2023-07-19 04:58:27 +0800

    update

--- Java-Web/src/main/java/com/service/UserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.service;
-
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserDetailsService {
-     UserDetails loadUserByUsername(String username);
-}
